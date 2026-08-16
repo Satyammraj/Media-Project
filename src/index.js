4405601ec506112ffa93bfa8278b2cab952ad963
Satyam Raj <samd89544@gmail.com>
2026-08-17 01:41:41 +0530

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,40 @@
+import dns from "dns";
+dns.setDefaultResultOrder('ipv4first');
+dns.setServers(['8.8.8.8', '1.1.1.1']);
+
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants.js"
+import connectDB from "./db/index.js"
+
+dotenv.config({
+    path: "./.env"
+})
+
+
+connectDB( )
+
+
+
+
+/*
+import express from "express";
+
+const app = express();
+
+;(async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+        app.on("error", (error) => {
+            console.error("Error connecting to MongoDB:", error)
+            throw error
+        })
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening on port ${process.env.PORT}`)
+        }
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error)
+        throw error
+    }
+})()
+    */
